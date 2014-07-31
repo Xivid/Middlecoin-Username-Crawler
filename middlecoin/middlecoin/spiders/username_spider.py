7d6b11e5ea8eee33c6b8d0fcd393c88a86a5b625
Yang Zhifei <xividyzf@gmail.com>
2014-07-31 18:39:19 +0800

    Finished
    output.json got.

--- middlecoin/middlecoin/spiders/username_spider.py
+++ middlecoin/middlecoin/spiders/username_spider.py
@@ -1,11 +1,14 @@
 import scrapy
 
+from middlecoin.items import MiddlecoinItem
+
 class UsernameSpider(scrapy.Spider):
 	name = "Username"
-	#allowed_domains = ['middlecoin.com']
+	#allowed_domains = ["middlecoin.com"]
 	start_urls = ["http://www.middlecoin.com/allusers.html"]
 
 	def parse(self, response):
-		filename = response.url.split("/")[-1]
-		with open(filename, 'wb') as f:
-			f.write(response.body)
+		for sel in response.xpath('//td[@id]'):
+			item = MiddlecoinItem()
+			item['Username'] = sel.xpath('text()').extract()
+			yield item
